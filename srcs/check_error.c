e23cc093c188b7329112a681b80e54715582fd7e
Adrian Jung <adrian.mael.jung@gmail.com>
2022-01-24 21:40:25 +0100

    push auto

--- srcs/check_error.c
+++ srcs/check_error.c
@@ -6,13 +6,13 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 18:23:39 by ajung             #+#    #+#             */
-/*   Updated: 2022/01/24 21:08:29 by ajung            ###   ########.fr       */
+/*   Updated: 2022/01/24 21:28:45 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	check_doublon(char **str)
+int	check_doublon(char **str)
 {
 	int	i;
 	int	j;
@@ -26,15 +26,16 @@ void	check_doublon(char **str)
 			if (ft_atoi(str[j]) == ft_atoi(str[i + j + 1]))
 			{
 				write(1, "Error\n", 6);
-				exit (0);
+				return (-1);
 			}
 			i++;
 		}
 		j++;
 	}
+	return (0);
 }
 
-void	check_error(char **str)
+int	check_error(char **str)
 {
 	int	i;
 	int j;
@@ -50,10 +51,12 @@ void	check_error(char **str)
 			else
 			{
 				write(1, "Error\n", 6);
-				exit (0);			
+				return (-1);			
 			}
 		}
 		i++;
 	}
-	check_doublon(str);
+	if (check_doublon(str) == -1)
+		return (-1);
+	return (0);
 }
--- srcs/operateur/push.c
+++ srcs/operateur/push.c
@@ -1,39 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_list.c                                        :+:      :+:    :+:   */
+/*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/24 19:31:58 by ajung             #+#    #+#             */
-/*   Updated: 2022/01/24 21:33:59 by ajung            ###   ########.fr       */
+/*   Created: 2022/01/24 21:15:24 by ajung             #+#    #+#             */
+/*   Updated: 2022/01/24 21:15:34 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-
-void	free_split(char **str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		free(str[i++]);
-	free(str);
-	exit (0);
-}
-void	free_list(t_list_ps **list)
-{
-	t_list_ps	*next_elem;
-
-	while((*list)->next)
-	{
-		next_elem = (*list)->next;
-		free(*list);
-		*list = next_elem;
-	}
-	free(*list);
-	list = NULL;
-	exit (0);
-}
